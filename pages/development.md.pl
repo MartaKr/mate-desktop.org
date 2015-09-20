1026ee88568a7bf851bd973236fcaf5f79b9ac9b
Marta <marta.krysik@gmail.com>
2015-09-20 22:51:35 +0200

    Translation

--- pages/development.md.pl
+++ pages/development.md.pl
@@ -3,24 +3,26 @@
 .. description:
 .. tags: Development
 .. date: 2011-12-05 12:00:30
-.. title: Development
-.. slug: development
+.. title: Rozwój
+.. slug: rozwój
 -->
 
-The git repositories for MATE are hosted at GitHub.
+Repozytorium git dla MATE jest hostowane na GitHub.
 
   * <https://github.com/mate-desktop>
 
-You can browse the code with cgit here too:
+Możesz przeglądać kod z cgit również tu:
 
   * <http://git.mate-desktop.org>
 
- Other useful links:
+ Inne użyteczne linki:
 
-  * [MATE University](/blog/2013-03-12-mate-university/)
-  * [MATE Development](http://wiki.mate-desktop.org/dev-doc)
-  * [MATE Roadmap](http://wiki.mate-desktop.org/roadmap)
+  * [Uniwersytet MATE](/blog/2013-03-12-mate-university/)
+  * [Rozwój MATE](http://wiki.mate-desktop.org/dev-doc)
+  * [Harmonogram MATE](http://wiki.mate-desktop.org/roadmap)
   * <http://wiki.mate-desktop.org/contribute>
   * <http://wiki.mate-desktop.org/translation>
   
-Don't hesitate to contribute and have fun improving MATE!
+Nie wahaj się, by kontrybuować, miej radość udoskonalając MATE!
+
+
